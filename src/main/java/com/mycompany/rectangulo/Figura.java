315e0d2ffd4a667d95c28be938afcaf997907734
camilocardona-ing <camicarsu07@gmail.com>
2025-10-10 18:40:49 -0500

    se creo la clase figura

---
+++ src/main/java/com/mycompany/rectangulo/Figura.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.rectangulo;
+
+/**
+ *
+ * @author camilocardonasuarez
+ */
+public class Figura {
+    private String nombre;
+
+    public Figura(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public double area() {
+        return 0.0;}
+
+
+    
+}
